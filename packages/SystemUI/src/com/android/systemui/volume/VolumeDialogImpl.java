0061f1a0e4519a8775655bdcf548d1945e11c3dc
NurKeinNeid <mralexman3000@gmail.com>
2020-08-28 21:04:04 +0200

    Revert "base: SystemUI: hide ringer button if alert slider is present."
    This reverts commit 95850a7094d61878dcde0f005f8d1e49a03358bf.

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -61,7 +61,6 @@ import android.os.VibrationEffect;
 import android.provider.Settings;
 import android.provider.Settings.Global;
 import android.text.InputFilter;
-import android.text.TextUtils;
 import android.util.Log;
 import android.util.Slog;
 import android.util.SparseBooleanArray;
@@ -167,8 +166,6 @@ public class VolumeDialogImpl implements VolumeDialog,
 
     private boolean mLeftVolumeRocker;
 
-    private boolean mHasAlertSlider;
-
     public VolumeDialogImpl(Context context) {
         mContext =
                 new ContextThemeWrapper(context, R.style.qs_theme);
@@ -181,7 +178,6 @@ public class VolumeDialogImpl implements VolumeDialog,
         mHasSeenODICaptionsTooltip =
                 Prefs.getBoolean(context, Prefs.Key.HAS_SEEN_ODI_CAPTIONS_TOOLTIP, false);
         mLeftVolumeRocker = mContext.getResources().getBoolean(R.bool.config_audioPanelOnLeftSide);
-        mHasAlertSlider = mContext.getResources().getBoolean(com.android.internal.R.bool.config_hasAlertSlider);
     }
 
     @Override
@@ -285,9 +281,7 @@ public class VolumeDialogImpl implements VolumeDialog,
             mDialogView.removeView(mODICaptionsTooltipViewStub);
             mODICaptionsTooltipViewStub = null;
         }
-        if (mHasAlertSlider) {
-            mRinger.setVisibility(View.GONE);
-        }
+
         if(!isAudioPanelOnLeftSide()) {
             mRinger.setForegroundGravity(Gravity.RIGHT);
         } else {
